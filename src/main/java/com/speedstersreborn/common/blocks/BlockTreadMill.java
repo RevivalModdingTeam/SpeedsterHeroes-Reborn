eef6c1a410fefc53392b456221928215f9f8650c
Josia50 <elwyn.vdb@gmail.com>
2019-04-06 18:00:02 +0200

    TeTreadMill

--- src/main/java/com/speedstersreborn/common/blocks/BlockTreadMill.java
+++ src/main/java/com/speedstersreborn/common/blocks/BlockTreadMill.java
@@ -7,17 +7,21 @@ import com.speedstersreborn.common.tileentity.TileTreadMill;
 import com.speedstersreborn.tabs.ModTabs;
 import com.speedstersreborn.util.handlers.IHasModel;
 import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class BlockTreadMill extends Block implements IHasModel {
+import javax.annotation.Nullable;
+
+public class BlockTreadMill extends Block implements IHasModel, ITileEntityProvider {
     public BlockTreadMill(String name, Material material) {
         super(material);
         setTranslationKey(name);
@@ -46,5 +50,16 @@ public class BlockTreadMill extends Block implements IHasModel {
     @Override
     public void registerModels() {
         SpeedsterHeroesReborn.proxy.registerItemRenderer(Item.getItemFromBlock(this),0, "inventory");
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta) {
+        return new TileTreadMill();
+    }
+
+    @Override
+    public boolean hasTileEntity(IBlockState state) {
+        return true;
     }
 }
