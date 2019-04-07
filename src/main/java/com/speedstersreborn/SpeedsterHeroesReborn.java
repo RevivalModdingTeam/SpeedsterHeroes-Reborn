b256e0059e7148239f8de46f8793c3f98e90c444
Josia50 <elwyn.vdb@gmail.com>
2019-04-07 09:13:53 +0200

    Fixed Depending Core (Always have the same or higher version) no refactor

--- src/main/java/com/speedstersreborn/SpeedsterHeroesReborn.java
+++ src/main/java/com/speedstersreborn/SpeedsterHeroesReborn.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = SpeedsterHeroesReborn.MODID, name = SpeedsterHeroesReborn.NAME, version = SpeedsterHeroesReborn.VERSION, dependencies = "required:revivalcore@[0.0.2,)")
+@Mod(modid = SpeedsterHeroesReborn.MODID, name = SpeedsterHeroesReborn.NAME, version = SpeedsterHeroesReborn.VERSION, dependencies = "required-after:revivalcore@[0.0.3,)")
 public class SpeedsterHeroesReborn
 {
     @Mod.Instance
