788e9dc3c4a86d8fc9e1c71dd3a1b3733d30c66f
Josia50 <elwyn.vdb@gmail.com>
2019-04-10 09:41:37 +0200

    Water Running & XP fixup

--- src/main/java/com/speedstersreborn/util/handlers/EventHandlePower.java
+++ src/main/java/com/speedstersreborn/util/handlers/EventHandlePower.java
@@ -4,6 +4,7 @@ package com.speedstersreborn.util.handlers;
 import com.revivalcore.util.helper.PlayerHelper;
 import com.speedstersreborn.common.capabilities.CapabilitySpeedster;
 import com.speedstersreborn.common.capabilities.ISpeedsterCap;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.living.LivingEvent;
@@ -15,6 +16,12 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class EventHandlePower {
 
     @SubscribeEvent
+    public static void MainPowers(LivingEvent.LivingUpdateEvent e) {
+        setXPAdd(e);
+        runWater(e);
+    }
+
+
     public static void setXPAdd(LivingEvent.LivingUpdateEvent e) {
         if (e.getEntity() instanceof EntityPlayer) {
             EntityPlayer player = (EntityPlayer) e.getEntity();
@@ -24,34 +31,49 @@ public class EventHandlePower {
                 if (cap.isSpeedster() && !player.capabilities.isCreativeMode) {
                     if (isMoving(player)) {
                         cap.setXP(cap.getXP() + 0.01 * cap.getSpeedLevel());
-                        System.out.println(cap.getXP());
                         PlayerHelper.sendMessage(player, "XP: " + cap.getXP(), true);
                     }
 
 
-                    if (cap.getXP() >= 100 && cap.getXP() < 101) {
+                    if (cap.getXP() >= 100 && cap.getXP() < 100.5) {
                         cap.setLevel(2);
                         PlayerHelper.sendMessage(player, "Level: " + cap.getLevel(), false);
                     }
 
-                    if (cap.getXP() >= 200 && cap.getXP() < 201) {
+                    if (cap.getXP() >= 200 && cap.getXP() < 200.5) {
                         cap.setLevel(3);
                         PlayerHelper.sendMessage(player, "Level: " + cap.getLevel(), false);
                     }
 
-                    if (cap.getXP() >= 300 && cap.getXP() < 301) {
+                    if (cap.getXP() >= 300 && cap.getXP() < 300.5) {
                         cap.setLevel(4);
                         PlayerHelper.sendMessage(player, "Level: " + cap.getLevel(), false);
                     }
 
-                    if (cap.getXP() >= 400 && cap.getXP() < 401) {
+                    if (cap.getXP() >= 400 && cap.getXP() < 400.5) {
                         cap.setLevel(5);
                         PlayerHelper.sendMessage(player, "Level: " + cap.getLevel(), false);
                     }
                 }
             }
         }
+    }
 
+
+    public static void runWater(LivingEvent.LivingUpdateEvent e) {
+        if (e.getEntity() instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) e.getEntity();
+            ISpeedsterCap cap = CapabilitySpeedster.get(player);
+
+            if (player.isSprinting() && cap.isSpeedster() && player.world.getBlockState(player.getPosition().add(0, -1, 0)).getBlock() instanceof BlockLiquid) {
+                {
+                    player.posY -= player.motionY;
+                    player.motionY = 0D;
+                    player.fallDistance = 0.0F;
+                    player.onGround = true;
+                }
+            }
+        }
     }
 
     public static boolean isMoving(EntityLivingBase entity) {
