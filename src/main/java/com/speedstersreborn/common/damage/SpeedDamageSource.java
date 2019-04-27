56bbde619d6ef2acec679a3c153f4bc272a49274
Josia50 <elwyn.vdb@gmail.com>
2019-04-27 11:01:16 +0200

    Damage Source

---
+++ src/main/java/com/speedstersreborn/common/damage/SpeedDamageSource.java
@@ -0,0 +1,8 @@
+package com.speedstersreborn.common.damage;
+
+import net.minecraft.util.DamageSource;
+
+public class SpeedDamageSource {
+
+  //  public static DamageSource EMPTY = new InitSource("empty", false);
+}
