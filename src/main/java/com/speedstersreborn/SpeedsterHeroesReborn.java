45bda920edfecd9c7abcf87f031ee05da4e302a7
Josia50 <elwyn.vdb@gmail.com>
2019-05-18 08:26:08 +0200

    Update JSON

--- src/main/java/com/speedstersreborn/SpeedsterHeroesReborn.java
+++ src/main/java/com/speedstersreborn/SpeedsterHeroesReborn.java
@@ -30,7 +30,7 @@ public class SpeedsterHeroesReborn
     public static final String MODID = "shr";
     public static final String NAME = "Speedsters Heroes Reborn";
     public static final String VERSION = "0.0.4";
-    public static final String UPDATEURL = "https://raw.githubusercontent.com/revivalmodding/SpeedsterHeroesReborn/master/update.json";
+    public static final String UPDATEURL = "https://raw.githubusercontent.com/RevivalModdingTeam/RevivalModding-ModBugs/master/updatespeedster.json";
 
 
     @EventHandler
