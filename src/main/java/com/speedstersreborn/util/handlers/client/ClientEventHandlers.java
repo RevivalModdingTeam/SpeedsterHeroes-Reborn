37a140141191150f04ff859ce3fd45689eb6fd03
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2019-05-21 09:05:04 +0200

    better speed bar, idk if it's working

--- src/main/java/com/speedstersreborn/util/handlers/client/ClientEventHandlers.java
+++ src/main/java/com/speedstersreborn/util/handlers/client/ClientEventHandlers.java
@@ -8,6 +8,8 @@ import com.revivalmodding.revivalcore.util.handlers.client.ClientEventHandler;
 import com.revivalmodding.revivalcore.util.helper.ImageHelper;
 import com.speedstersreborn.SpeedsterHeroesReborn;
 import com.speedstersreborn.api.SpeedAPI;
+import com.speedstersreborn.common.capabilities.CapabilitySpeedster;
+import com.speedstersreborn.common.capabilities.ISpeedsterCap;
 import com.speedstersreborn.network.NetworkHandler;
 import com.speedstersreborn.network.packets.speedstercap.PacketSetPhasing;
 import com.speedstersreborn.network.packets.speedstercap.PacketSetSpeed;
@@ -17,6 +19,11 @@ import com.speedstersreborn.util.config.CFGOverlayPosition;
 import com.speedstersreborn.util.config.SHRConfig;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -31,7 +38,7 @@ import net.minecraftforge.fml.relauncher.Side;
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientEventHandlers {
 
-	static final ResourceLocation SPEED_INDICATOR = new ResourceLocation(SpeedsterHeroesReborn.MODID + ":textures/overlay/speed_indicators.png");
+	static final ResourceLocation SPEED_INDICATOR = new ResourceLocation(SpeedsterHeroesReborn.MODID + ":textures/overlay/speedbar.png");
 	
 	static float speed;
 	static int x;
@@ -39,7 +46,7 @@ public class ClientEventHandlers {
 	@SubscribeEvent
 	public static void keyPressed(InputEvent.KeyInputEvent e) {
         IMetaCap metaCap = CapabilityMeta.get(Minecraft.getMinecraft().player);
-
+        
         if (metaCap.hasMetaPowers() && MetaHelper.getMetaPowerName(metaCap.getMetaPower()) == MetaPowerStrings.SPEEDSTER) {
 
             if (ClientEventHandler.ENABLE.isPressed()) {
@@ -66,9 +73,8 @@ public class ClientEventHandlers {
     	if(player == null) {
     		return;
     	}
-    	
-    	IMetaCap cap = CapabilityMeta.get(player);
-    	if(cap.hasMetaPowers() && MetaHelper.getMetaPowerName(cap.getMetaPower()) == MetaPowerStrings.SPEEDSTER) {
+    	ISpeedsterCap cap = CapabilitySpeedster.get(player);
+    	if(cap.isSpeedster()) {
     		updateSpeed(SpeedAPI.isPlayerMoving(player) ? player.getAIMoveSpeed() : 0f);
     	}
     }
@@ -77,25 +83,65 @@ public class ClientEventHandlers {
 	public static void onRenderPost(RenderGameOverlayEvent.Post e) {
 		Minecraft mc = Minecraft.getMinecraft();
 		if(e.getType() == ElementType.ALL) {
-			IMetaCap cap = CapabilityMeta.get(mc.player);
-			if(cap.hasMetaPowers() && MetaHelper.getMetaPowerName(cap.getMetaPower()) == MetaPowerStrings.SPEEDSTER) {
-				doSpeedRender(mc, e.getResolution().getScaledHeight());
+			ISpeedsterCap cap = CapabilitySpeedster.get(mc.player);
+			if(cap.isSpeedster()) {
+				doSpeedRender(mc, e.getResolution(), cap.getSpeedLevel() - 1);
 			}
 		}
 	}
 	
-	private static void doSpeedRender(Minecraft mc, int pixHeight) {
+	private static void doSpeedRender(Minecraft mc, ScaledResolution res, int level) {
 		CFGOverlayPosition pos = SHRConfig.speedstersHeroesReborn.speedIndicator;
-		ImageHelper.drawImageWithUV(mc, SPEED_INDICATOR, pos.x, pixHeight-pos.y, 64, 10, 0, 0, 1, 0.3125D, false);
-		float progress = speed / (SpeedAPI.MaxSpeedLevel*SHRConfig.speedstersHeroesReborn.speedIncreaseOverLevel);
-		x = IndicatorAnimation.move(x, (int)(64*progress), 1.5f);
-		ImageHelper.drawImageWithUV(mc, SPEED_INDICATOR, pos.x + x, pixHeight-pos.y, 8, 16, 0, 0.34375, 0.1875, 0.6875, true);
+		int width = res.getScaledWidth();
+		int height = res.getScaledHeight();
+		int left = width / 2 - 63;
+		int top = height - 62;
+		ImageHelper.drawImageWithUV(mc, SPEED_INDICATOR, left, top-pos.y, 128, 10, 0, 0, 1, 0.716D, false);
+		
+		if(level < 0)
+			return;
+		x = IndicatorAnimation.move(x, (int)(level*6.4), 0.5f);
+		drawImageWithUV(mc, SPEED_INDICATOR, left-1 + pos.x + x, top+8-pos.y, 8, 4, 0, 0.733, 0.05197505197, 0.9333, true);
 		
 	}
 	
 	private static void updateSpeed(float newSpeed) {
 		speed = newSpeed;
 	}
+	
+	// TODO change the startX,Y to double inside ImageHelper
+    public static void drawImageWithUV(Minecraft minecraft, ResourceLocation imageLocation, double startX, double startY, double width, double height, double startU, double startV, double u, double v, boolean transparent)
+    {
+        minecraft.getTextureManager().bindTexture(imageLocation);
+        GlStateManager.color(1f, 1f, 1f);
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder buffer = tessellator.getBuffer();
+
+        if(u > 1) u = 1;
+        if(v > 1) v = 1;
+
+        prepareShape(buffer, startX, startY, width, height, startU, startV, u, v);
+
+        if(transparent)
+        {
+            GlStateManager.enableAlpha();
+            GlStateManager.enableBlend();
+            tessellator.draw();
+            GlStateManager.disableBlend();
+            GlStateManager.disableAlpha();
+        }
+
+        else tessellator.draw();
+    }
+    
+    private static void prepareShape(BufferBuilder buffer, double startX, double startY, double width, double height, double startU, double startV, double endU, double endV)
+    {
+    	buffer.begin(7, DefaultVertexFormats.POSITION_TEX);
+        buffer.pos(startX, startY + height, 0).tex(startU, endV).endVertex();
+        buffer.pos(startX + width, startY + height, 0).tex(endU, endV).endVertex();
+        buffer.pos(startX + width, startY, 0).tex(endU, startV).endVertex();
+        buffer.pos(startX, startY, 0).tex(startU, startV).endVertex();
+    }
 	
 	public static class IndicatorAnimation {
 		
