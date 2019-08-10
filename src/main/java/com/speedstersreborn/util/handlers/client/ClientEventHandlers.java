d51ffc536594e76d3ff630a893588a996e34f59f
Josia50 <elwyn.vdb@gmail.com>
2019-08-10 12:24:54 +0200

    Vibrating

--- src/main/java/com/speedstersreborn/util/handlers/client/ClientEventHandlers.java
+++ src/main/java/com/speedstersreborn/util/handlers/client/ClientEventHandlers.java
@@ -10,61 +10,88 @@ import com.speedstersreborn.util.config.CFGSpeedIndicatorUnit;
 import com.speedstersreborn.util.config.SHRConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
+import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
 import java.text.DecimalFormat;
+import java.util.Random;
 
 
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientEventHandlers {
 
-	static final ResourceLocation SPEED_INDICATOR = new ResourceLocation(SpeedsterHeroesReborn.MODID + ":textures/overlay/speedbar.png");
-	static final DecimalFormat FORMAT = new DecimalFormat("#####,##0.00");
-	
-	static double x;
-	
-	@SubscribeEvent
-	public static void onRenderPost(RenderGameOverlayEvent.Post e) {
-		Minecraft mc = Minecraft.getMinecraft();
-		if(e.getType() == ElementType.ALL) {
-			ISpeedsterCap cap = CapabilitySpeedster.get(mc.player);
-			if(cap.isSpeedster() || cap.hasVelocity()) {
-				doSpeedRender(mc, e.getResolution(), cap.getSpeedLevel() - 1);
-			} else if(x != 0) {
-				x = 0;
-			}
-		}
-	}
-	
-	private static void doSpeedRender(Minecraft mc, ScaledResolution res, int level) {
-		if(level < 0 || mc.player.isCreative() || mc.player.isSpectator()) return;
-		CFGOverlayPosition pos = SHRConfig.speedstersHeroesReborn.speedIndicator;
-		CFGSpeedIndicatorUnit speedUnit = SHRConfig.speedstersHeroesReborn.speedUnit;
-		int width = res.getScaledWidth();
-		int height = res.getScaledHeight();
-		int left = width / 2 - 64;
-		int top = height - 62;
-		ImageHelper.drawImageWithUV(mc, SPEED_INDICATOR, left + pos.x, top-pos.y, 128, 10, 0, 0, 1, 0.71428571428D, true);
-		x = IndicatorAnimation.move(x, level*6.4f, 0.5f);
-		ImageHelper.drawImageWithUV(mc, SPEED_INDICATOR, left + pos.x + x, top+8-pos.y, 6, 3, 0, 0.73, 0.05, 1, true);
-		mc.fontRenderer.drawStringWithShadow(FORMAT.format((SpeedAPI.getPlayerMovementSpeed(mc.player)*20)*speedUnit.getMultiplier())+ " " + speedUnit.getName(), left + pos.x, top-pos.y - 9, 0xFFFFFF);
-		
-	}
-	
-	public static class IndicatorAnimation {
-		
-		public static double interpolate(double base) {
-			return base * 60 / Minecraft.getDebugFPS();
-		}
-		
-		public static double move(double current, float target, float modifier) {
-			double d = Math.abs(current - target) < modifier ? target : current < target ? current + interpolate(modifier) : current - interpolate(modifier);
-			return d;
-		}
-	}
+    static final ResourceLocation SPEED_INDICATOR = new ResourceLocation(SpeedsterHeroesReborn.MODID + ":textures/overlay/speedbar.png");
+    static final DecimalFormat FORMAT = new DecimalFormat("#####,##0.00");
+
+    static double x;
+
+    @SubscribeEvent
+    public static void onRenderPost(RenderGameOverlayEvent.Post e) {
+        Minecraft mc = Minecraft.getMinecraft();
+        if (e.getType() == ElementType.ALL) {
+            ISpeedsterCap cap = CapabilitySpeedster.get(mc.player);
+            if (cap.isSpeedster() || cap.hasVelocity()) {
+                doSpeedRender(mc, e.getResolution(), cap.getSpeedLevel() - 1);
+            } else if (x != 0) {
+                x = 0;
+            }
+        }
+    }
+
+    private static void doSpeedRender(Minecraft mc, ScaledResolution res, int level) {
+        if (level < 0 || mc.player.isCreative() || mc.player.isSpectator()) return;
+        CFGOverlayPosition pos = SHRConfig.speedstersHeroesReborn.speedIndicator;
+        CFGSpeedIndicatorUnit speedUnit = SHRConfig.speedstersHeroesReborn.speedUnit;
+        int width = res.getScaledWidth();
+        int height = res.getScaledHeight();
+        int left = width / 2 - 64;
+        int top = height - 62;
+        ImageHelper.drawImageWithUV(mc, SPEED_INDICATOR, left + pos.x, top - pos.y, 128, 10, 0, 0, 1, 0.71428571428D, true);
+        x = IndicatorAnimation.move(x, level * 6.4f, 0.5f);
+        ImageHelper.drawImageWithUV(mc, SPEED_INDICATOR, left + pos.x + x, top + 8 - pos.y, 6, 3, 0, 0.73, 0.05, 1, true);
+        mc.fontRenderer.drawStringWithShadow(FORMAT.format((SpeedAPI.getPlayerMovementSpeed(mc.player) * 20) * speedUnit.getMultiplier()) + " " + speedUnit.getName(), left + pos.x, top - pos.y - 9, 0xFFFFFF);
+
+    }
+
+    public static class IndicatorAnimation {
+
+        public static double interpolate(double base) {
+            return base * 60 / Minecraft.getDebugFPS();
+        }
+
+        public static double move(double current, float target, float modifier) {
+            double d = Math.abs(current - target) < modifier ? target : current < target ? current + interpolate(modifier) : current - interpolate(modifier);
+            return d;
+        }
+    }
+
+
+    public static boolean vibrating = false;
+
+    @SubscribeEvent
+    public static void renderPlayerVibratingPre(RenderPlayerEvent.Pre e) {
+        Random r = new Random();
+        if (vibrating) {
+            GlStateManager.pushMatrix();
+            for (int i = 0; i < 2; i++) {
+                GlStateManager.translate((r.nextFloat() - 0.5F) / 15, 0, (r.nextFloat() - 0.5F) / 15);
+                GlStateManager.color(1, 1, 1, 0.4F);
+            }
+            GlStateManager.enableBlend();
+        }
+    }
+
+    @SubscribeEvent
+    public static void renderPlayerVibratingPost(RenderPlayerEvent.Post e) {
+        if (vibrating) {
+            GlStateManager.disableBlend();
+            GlStateManager.popMatrix();
+        }
+    }
 }
