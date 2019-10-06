211503e99297036768efde8204876624176ba550
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2019-10-06 18:22:00 +0200

    needs 0.3.7 core version

--- src/main/java/com/speedstersreborn/SpeedsterHeroesReborn.java
+++ src/main/java/com/speedstersreborn/SpeedsterHeroesReborn.java
@@ -29,7 +29,7 @@ public class SpeedsterHeroesReborn
     public static final String MODID = "shr";
     public static final String NAME = "Speedsters Heroes Reborn";
     public static final String VERSION = "0.0.7";
-    public static final String REQUIRED = "required-after:revivalcore@[0.3.3,)";
+    public static final String REQUIRED = "required-after:revivalcore@[0.3.7,)";
     public static final String UPDATEURL = "https://raw.githubusercontent.com/RevivalModdingTeam/RevivalModding-ModBugs/master/update/updatespeedster.json";
 
 
